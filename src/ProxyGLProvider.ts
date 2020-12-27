1d46de3e01042f349888e95c14e0da2a10676706
Aaron Faanes <dafrito@gmail.com>
2020-12-26 23:01:17 -0600

    Add ProxyGLProvider for easy usage in e.g. painters

---
+++ src/ProxyGLProvider.ts
@@ -0,0 +1,31 @@
+import GLProvider from './GLProvider';
+
+export default abstract class ProxyGLProvider implements GLProvider {
+  _glProvider:GLProvider;
+
+  constructor(window:GLProvider) {
+    this._glProvider = window;
+  }
+
+  glProvider():GLProvider {
+    return this._glProvider;
+  }
+
+  abstract id():string;
+
+  shaders():{ [shaderName:string]:WebGLProgram } {
+    return this.glProvider().shaders();
+  }
+
+  container():HTMLElement {
+    return this.glProvider().container();
+  }
+
+  canvas():HTMLCanvasElement {
+    return this.glProvider().canvas();
+  }
+
+  gl():WebGLRenderingContext {
+    return this.glProvider().gl();
+  }
+}
